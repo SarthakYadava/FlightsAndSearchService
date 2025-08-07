ff1db5898cfa44e7287aac5ba83e94b1271f0707
SarthakYadava <sarthkyadav10r@gmail.com>
2025-08-08 01:10:31 +0530

    Added flights search crud with few filters

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -12,6 +12,7 @@ class FlightService {
             if(!compareTime(data.arrivalTime, data.departureTime)) {
              throw {error: 'Arrival time cannot be less than departure time'};
                 }
+
          const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
         const flight = await this.flightRepository.createFlight({
         ...data, totalSeats:airplane.capacity
@@ -24,7 +25,18 @@ return flight;
             throw {error};
         }
     }
+
+    async getAllFlightData(data) {
+    try {
+        const flights = await this.flightRepository.getAllFlights(data);
+        return flights;
+    } catch (error) {
+        console.log("Something went wrong at service layer");
+        throw {error};
+    }
+ }
 }
+
 
 module.exports = FlightService;
 /**
